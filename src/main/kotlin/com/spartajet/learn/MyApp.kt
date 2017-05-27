c8f79196be5c9da11429de4c3a367215ed6fe7d4
spartajet <spartajet.guo@gmail.com>
2017-05-28 00:14:45 +0200

    implement tornadofx+springboot by the help form edvin

--- src/main/kotlin/com/spartajet/learn/MyApp.kt
+++ src/main/kotlin/com/spartajet/learn/MyApp.kt
@@ -1,17 +1,33 @@
 package com.spartajet.learn
 
 import javafx.application.Application
-import org.springframework.stereotype.Component
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import tornadofx.*
-import javax.annotation.PostConstruct
+import kotlin.reflect.KClass
 
 /**
  * @description
  * @create 2017-05-27 下午5:47
  * @email spartajet.guo@gmail.com
  */
-@Component class MyApp : App(MainView::class) {
-    @PostConstruct fun load() {
-        Application.launch(MyApp::class.java)
+@SpringBootApplication class MyApp : App(MainView::class) {
+
+
+    override fun init() {
+        super.init()
+        val applicationContext = SpringApplication.run(MyApp::class.java);
+        FX.dicontainer = object : DIContainer {
+            override fun <T : Any> getInstance(type: KClass<T>): T = applicationContext.getBean(type.java)
+            override fun <T : Any> getInstance(type: KClass<T>, name: String): T = applicationContext.getBean(type.java, name)
+        }
     }
+
+//    @PostConstruct fun load() {
+//        Application.launch(MyApp::class.java)
+//    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(MyApp::class.java)
 }
